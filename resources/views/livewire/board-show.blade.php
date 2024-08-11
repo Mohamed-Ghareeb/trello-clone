725b4ff520f8f5338c6523c89ee6004ae9e84d7b
mohamed-ghareeb <mohamed.ghareeb725@gmail.com>
2024-08-11 23:18:59 +0300

    settup the project and make the design of boards

---
+++ resources/views/livewire/board-show.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ $board->title }}
+        </h2>
+    </x-slot>
+    <div class="w-full p-6 overflow-x-scroll">
+        <div class="flex w-max space-x-6 h-[calc(theme('height.screen')-64px-73px-theme('padding.12'))]">
+            @foreach (range(1, 10) as $column)
+                <livewire:column />
+            @endforeach
+        </div>
+    </div>
+</div>
